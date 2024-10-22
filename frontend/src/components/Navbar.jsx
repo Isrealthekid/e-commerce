8966e675e73561febe3a24a255f6b40419f0b3bd
isrealthekid <oluwoleisreal12@gmail.com>
2024-10-22 16:29:16 +0100

    BestSeller

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,11 @@ const Navbar = () => {
 
   return (
     <div className='flex items-center justify-between py-5 font-medium' >
+
+      <Link to='/'> 
         <img src={assets.logo} alt="logo"  className='w-36'/>
+      </Link>
+        
 
   
 
