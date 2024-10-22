ff25f458e562c838d806892ae32d40734db12584
isrealthekid <oluwoleisreal12@gmail.com>
2024-10-22 17:01:09 +0100

    OurPolicySection

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
+import OurPoilcy from '../components/OurPoilcy'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <LatestCollection/>
       <BestSeller/>
+      <OurPoilcy/>
     </div>
   )
 }
