a3fc10dea5fac13fdf70c29d56e2b72a86ad1ff0
isrealthekid <oluwoleisreal12@gmail.com>
2024-10-29 21:17:08 +0100

    ContactUs

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className="text-2xl text-center pt-10 border-t">
+        <Title text1={'CONTACT'} text2={'US'} />
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28" >
+        <img src={assets.contact_img} className='w-full md:max-w-[480px]' alt="" />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className='text-gray-500'>11, Ganza Industrial Estate, <br /> Suite 150 chengen, chezh State</p>
+          <p className='text-gray-500'>Tel: (234) 123-443-4442 <br /> Email: info@foreveryours.com</p>
+          <p className='font-semibold text-xl text-gray-600'>Careers at Forever Yours</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+
+      </div>
+
+  </div>
+
+
   )
 }
 
