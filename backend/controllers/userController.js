7ee5345e2b52e379a624c8ac3ce2af235a767bf8
isrealthekid <oluwoleisreal12@gmail.com>
2024-10-30 13:40:56 +0100

    backend setup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,20 @@
+
+// route for user login
+const loginUser = async (req,res) => {
+
+}
+
+
+// route for user registratiion
+const registerUser = async (req,res) => {
+    res.json({msg:'Register api'})
+}
+
+
+// admin login
+
+const adminLogin = async (req,res) => {
+
+}
+
+export {loginUser, registerUser, adminLogin}
